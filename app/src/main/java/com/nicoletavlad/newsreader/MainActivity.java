a8012232c7ccc894bf9dd64e3e13f0611ffc599c
nicovlad16 <nicovlad16@gmail.com>
2020-05-05 14:10:10 +0300

    changed to Java project

---
+++ app/src/main/java/com/nicoletavlad/newsreader/MainActivity.java
@@ -0,0 +1,24 @@
+package com.nicoletavlad.newsreader;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.nicoletavlad.newsreader.ui.main.MainFragment;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+        if (savedInstanceState == null)
+        {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow();
+        }
+    }
+}
---
+++ app/src/main/java/com/nicoletavlad/newsreader/ui/main/MainViewModel.java
@@ -0,0 +1,8 @@
+package com.nicoletavlad.newsreader.ui.main;
+
+import androidx.lifecycle.ViewModel;
+
+public class MainViewModel extends ViewModel
+{
+    // TODO: Implement the ViewModel
+}
